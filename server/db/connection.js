64f99eaeff6b869661f911ab903b48f49d89fdf3
KevinDMort <kevi5221@edu.zealand.dk>
2024-05-20 21:14:35 +0200

    fjernet sql logging

--- server/db/connection.js
+++ server/db/connection.js
@@ -2,18 +2,13 @@ import knex from 'knex';
 import { v4 as uuidv4 } from 'uuid';
 
 export const connection = knex({
-  client: 'sqlite3',
+  client: 'sqlite3', // Specify the SQL database client
   connection: {
     filename: './poster.db'
   },
-  useNullAsDefault: true,
-  debug: true // Enable debugging
+  useNullAsDefault: true
 });
 
-connection.on('query', (query) => {
-  console.log('SQL Query:', query.sql);
-  console.log('Bindings:', query.bindings);
-});
 export function generateID()
 {
   return uuidv4(); 
